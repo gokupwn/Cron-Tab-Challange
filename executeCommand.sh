f3dc427b559cc4d426561de17aa093288e06f132
Hassan-Al-Achek <76757267+Hassan-Al-Achek@users.noreply.github.com>
2021-03-31 22:15:49 +0300

    Update executeCommand.sh

--- executeCommand.sh
+++ executeCommand.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+# change username to the normal user name
+log=/home/username/Demo/logs/hackers
 
-log=/home/$USER/Demo/logs/hackers
-
-cd /home/$USER/Demo/
+cd /home/username/Demo/
 
 cat $log | cut -d' ' -f3- | sort -u | while read ip; do
     sh -c "nmap --top-ports 10 -oN recon/${ip}.nmap ${ip}"
